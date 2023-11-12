5ed16ad0b8c789db671f2b2b0769f44a89116ff7
Alex Mangust <sanya.stogny@gmail.com>
2023-11-13 03:28:56 +0800

    Изменил 5 пункт меню

--- option.py
+++ option.py
@@ -57,7 +57,6 @@ def find_phone_family(file_open):
 
 # 3. Найти абонента по номеру телефона
 def find_subscriber_by_phone(file_open):
-    # phone = input("Введите номер телефона абонента: ")
     phone_number = StringVar()
     phone = option_two.input_phone(phone_number)
     found_phone_record = []
@@ -112,68 +111,50 @@ def add_subscriber_in_phonebook(file_open, filename):
     win.geometry('500x500')
     canvas = Canvas(win,height=20,width = 10)
     canvas.pack()
-    frame1 = Frame(win, bg='red')
+    frame1 = Frame(win, bg='#ffe4b5')
     frame1.place(relx=0.45, rely=0.40, relwidth=0.1, relheight=0.1)
     btn = Button(frame1, text="ОК", command=lambda: ok_function(win))
     btn.pack()
 
+
 # 5. Изменить контакт
 def change(file_open, filename):
-    next_change = True
-    while (next_change):
-        fields = ['Фамилия', 'Имя', 'Телефон', 'Описание']
-        q = False
-        while q == False:
-            print("По какому параметру хотите производить поиск?\n1. Фамилия\n2. Имя \n3. Телефон\n4. Описание")
-            q=True
-            field_input = int(input())
-            field_in = fields[field_input - 1]
-            famile_found = input("Введите свой запрос: ")
-            if field_input==1 or field_input==2:
-                famile_found = Higher_order(famile_found)
-            found_people = []
-            id = []
-            for record in range(len(file_open)):
-                if file_open[record].get(field_in) == famile_found:
-                    found_people.append(file_open[record])
-                    id.append(record)
-            if len(found_people) >= 2:
-                print("Найдено больше одного запроса, выберите 1")
-                for i in range(len(found_people)):
-                    line = ', '.join('{} {}'.format(key, val) for key, val in sorted(found_people[i].items()))
-                    print(f"{i+1} - {line}")
-                vi = int(input())
-                vi = vi - 1
-            elif len(found_people) == 0:
-            
-                print("Запись не найдена!")
-                print("Чтобы повторить изменение контакта нажмите Enter")
-                print("Чтобы выйти в главное меню введите любой символ")
-                num = input("")
-                if num == "":
-                    q=False
-                else:
-                    return 0
-        if len(found_people) == 1:
-            vi = 0
-                
-        print("Что вы хотите изменить?\n Фамилия, нажмите 1\n Имя, нажмите 2 \n Телефон, нажмите 3\n Описание, нажмите 4")
-        change = int(input())
-        change_in = fields[change - 1]
-        new_record = input("Введите новое значение: ")
-        if change==1 or change==2:
-                new_record = Higher_order(new_record)
-        for record in file_open:
-            if record == found_people[vi]:
-                record[change_in] = new_record
+    fields = ['Фамилия', 'Имя', 'Телефон', 'Описание']
+    field_in = fields[option_two.find_contact_to_change() - 1]
+    family_found = option_two.contact_change(file_open, filename, field_in)
+    if field_in == 1 or field_in == 2:
+        family_found = Higher_order(family_found)
+    found_people = []
+    id = []
+    for record in range(len(file_open)):
+        if file_open[record].get(field_in) == family_found:
+            found_people.append(file_open[record])
+            id.append(record)
+    
+    if len(found_people) >= 2:
+        choice = option_two.many_find(found_people) - 1
+    elif len(found_people) == 0:
+        if option_two.not_found() == True:
+            change(file_open, filename)
+        else:
+            return 0
+    if len(found_people) == 1:
+        choice = 0
+    your_change = fields[option_two.choice_contact_to_change() - 1]
+    new_record = option_two.contact_change_end(file_open, filename, your_change)
+    if your_change == 1 or your_change == 2:
+        new_record = Higher_order(new_record)
+    for record in file_open:
+        if record == found_people[choice]:
+            record[your_change] = new_record
         with open(filename, 'w', encoding='utf-8') as ph:
             for record in file_open:
                 line = ','.join(record[field] for field in fields)
                 ph.write(line + "\n")
-        next_input = int(input("Хотите поменять что-либо еще?\n1. Да   2. Нет\n"))
-        if next_input == 2:
-            next_change = False
-
+    if option_two.saved_change() == True:
+        change(file_open, filename)
+    else:
+        return 0
 
 # 6. Удалить запись
 def delete_record(file_open, filename):
@@ -223,6 +204,9 @@ def Higher_order(input_user):
     input_user = list(input_user)
     input_user[0] = input_user[0].upper()
     return ''.join(input_user)
+
+
+
 
 
 
--- option_two.py
+++ option_two.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 
+
+# 2. Найти телефон по фамилии
 def input_family(last_name):
         def find_family():
             family = entry.get()
@@ -31,6 +33,7 @@ def input_family(last_name):
         return last_name.get()
 
 
+# 3. Найти абонента по номеру телефона
 def input_phone(phone_number):
         def find_family():
             family = entry.get()
@@ -60,7 +63,7 @@ def input_phone(phone_number):
         win.wait_window(win)
         return phone_number.get()
 
-
+# 4. Добавить абонента в справочник
 def create_contact():
         def create_people(family, name, phone, info):
             family = family.get()
@@ -113,7 +116,223 @@ def create_contact():
 
 
 
-# family.set(result_var.get())
-# name.set(result_var.get())
-# phone.set(result_var.get())
-# info.set(result_var.get())
+# 5. Изменить контакт
+def find_contact_to_change():
+        def input_family():
+            result_var.set(1)
+            win.destroy()
+        def input_name():
+            result_var.set(2)
+            win.destroy()
+        def input_phone():
+            result_var.set(3)
+            win.destroy()
+        def input_info():
+            result_var.set(4)
+            win.destroy()
+        
+        win = Tk()
+        win['bg'] = '#ffe4b5'
+        win.title('Изменение контакта')
+        win.wm_attributes('-alpha', 1.0)
+        win.geometry('500x500')
+        canvas = Canvas(win, height=20, width=10)
+        canvas.pack()
+
+        result_var = IntVar()
+
+        title = Label(win, text='По какому критерию хотите произвести поиск?', bg='#ffe4b5', font=60)
+        title.pack()
+
+        frame1 = Frame(win, bg='#ffe4b5')
+        frame1.place(relx=0.35, rely=0.28, relwidth=0.3, relheight=0.2)
+        btn = Button(frame1, text="По фамилии", command=lambda: input_family())
+        btn.pack()
+        frame2 = Frame(win, bg='#ffe4b5')
+        frame2.place(relx=0.35, rely=0.38, relwidth=0.3, relheight=0.2)
+        btn = Button(frame2, text="По имени", command=lambda: input_name())
+        btn.pack()
+        frame3 = Frame(win, bg='#ffe4b5')
+        frame3.place(relx=0.35, rely=0.48, relwidth=0.3, relheight=0.2)
+        btn = Button(frame3, text="По номеру телефона", command=lambda: input_phone())
+        btn.pack()
+        frame4 = Frame(win, bg='#ffe4b5')
+        frame4.place(relx=0.35, rely=0.58, relwidth=0.3, relheight=0.2)
+        btn = Button(frame4, text="По описанию", command=lambda: input_info())
+        btn.pack()
+        win.wait_window(win)
+        return result_var.get()
+
+
+def contact_change(file_open, filename, field_in):
+        def your_input():
+            result_var.set(your_choice.get())
+            win.destroy()
+        win = Tk()
+        win['bg'] = '#ffe4b5'
+        win.title('Изменение контакта')
+        win.wm_attributes('-alpha', 1.0)
+        win.geometry('500x500')
+        canvas = Canvas(win, height=20, width=10)
+        canvas.pack()
+        result_var = StringVar()
+        title = Label(win, text='Введите свой запрос', bg='#ffe4b5', font=60)
+        title.pack()
+        your_choice = Entry(win, bg='white')
+        your_choice.pack()
+        frame = Frame(win, bg='#ffe4b5')
+        frame.place(relx=0.35, rely=0.28, relwidth=0.3, relheight=0.2)
+        btn = Button(frame, text="Ввод", command=lambda: your_input())
+        btn.pack()
+        win.wait_window(win)
+        return result_var.get()
+
+def many_find(found_people):
+        def choice():
+            result_var.set(your_choice.get())
+            win.destroy()
+        win = Tk()
+        win['bg'] = '#ffe4b5'
+        win.title('Найдено больше одного запроса!')
+        win.wm_attributes('-alpha', 1.0)
+        win.geometry('800x800')
+        canvas = Canvas(win, height=20, width=10)
+        canvas.pack()
+        result_var = IntVar()
+        for i in range(len(found_people)):
+            line = ', '.join('{} {}'.format(key, val) for key, val in sorted(found_people[i].items()))
+            title = Label(win, text=f"{i+1}.  {line}", bg='#ffe4b5', font=60)
+            title.pack()
+        title = Label(win, text='Ввведите номер нужного вам контакта', bg='#ffe4b5', font=60)
+        title.pack()
+        your_choice = Entry(win, bg='white')
+        your_choice.pack()
+        frame = Frame(win, bg='#ffe4b5')
+        frame.place(relx=0.35, rely=0.28, relwidth=0.3, relheight=0.2)
+        btn = Button(frame, text="Ввод", command=lambda: choice())
+        btn.pack()
+        win.wait_window(win)
+        return result_var.get()
+
+
+def not_found():
+        def yes_function(window):
+            result_var.set(True)
+            window.destroy()
+        def no_function(window):
+            window.destroy()
+        win = Tk()
+        win['bg'] = '#ffe4b5'
+        win.title('Поиск не дал результата!')
+        win.wm_attributes('-alpha', 1.0)
+        win.geometry('500x500')
+        canvas = Canvas(win, height=20, width=10)
+        canvas.pack()
+        result_var = BooleanVar()
+        title = Label(win, text='Хотите попробовать снова?', bg='#ffe4b5', font=60)
+        title.pack()
+        frame = Frame(win, bg='white')
+        frame.place(relx=0.45, rely=0.40, relwidth=0.1, relheight=0.1)
+        btn = Button(frame, text="Да", command=lambda: yes_function(win))
+        btn.pack()
+        btn = Button(frame, text="Нет", command=lambda: no_function(win))
+        btn.pack()
+        win.wait_window(win)
+        return result_var.get()
+
+
+def choice_contact_to_change():
+        def input_family():
+            result_var.set(1)
+            win.destroy()
+        def input_name():
+            result_var.set(2)
+            win.destroy()
+        def input_phone():
+            result_var.set(3)
+            win.destroy()
+        def input_info():
+            result_var.set(4)
+            win.destroy()
+        
+        win = Tk()
+        win['bg'] = '#ffe4b5'
+        win.title('Изменение контакта')
+        win.wm_attributes('-alpha', 1.0)
+        win.geometry('500x500')
+        canvas = Canvas(win, height=20, width=10)
+        canvas.pack()
+
+        result_var = IntVar()
+
+        title = Label(win, text='Что вы хотите изменить?', bg='#ffe4b5', font=60)
+        title.pack()
+
+        frame1 = Frame(win, bg='#ffe4b5')
+        frame1.place(relx=0.35, rely=0.28, relwidth=0.3, relheight=0.2)
+        btn = Button(frame1, text="Фамилию", command=lambda: input_family())
+        btn.pack()
+        frame2 = Frame(win, bg='#ffe4b5')
+        frame2.place(relx=0.35, rely=0.38, relwidth=0.3, relheight=0.2)
+        btn = Button(frame2, text="Имя", command=lambda: input_name())
+        btn.pack()
+        frame3 = Frame(win, bg='#ffe4b5')
+        frame3.place(relx=0.35, rely=0.48, relwidth=0.3, relheight=0.2)
+        btn = Button(frame3, text="Номеру телефона", command=lambda: input_phone())
+        btn.pack()
+        frame4 = Frame(win, bg='#ffe4b5')
+        frame4.place(relx=0.35, rely=0.58, relwidth=0.3, relheight=0.2)
+        btn = Button(frame4, text="Описание", command=lambda: input_info())
+        btn.pack()
+        win.wait_window(win)
+        return result_var.get()
+
+
+def contact_change_end(file_open, filename, field_in):
+        def your_input():
+            result_var.set(your_choice.get())
+            win.destroy()
+        win = Tk()
+        win['bg'] = '#ffe4b5'
+        win.title('Изменение контакта')
+        win.wm_attributes('-alpha', 1.0)
+        win.geometry('500x500')
+        canvas = Canvas(win, height=20, width=10)
+        canvas.pack()
+        result_var = StringVar()
+        title = Label(win, text='Введите новое значение', bg='#ffe4b5', font=60)
+        title.pack()
+        your_choice = Entry(win, bg='white')
+        your_choice.pack()
+        frame = Frame(win, bg='#ffe4b5')
+        frame.place(relx=0.35, rely=0.28, relwidth=0.3, relheight=0.2)
+        btn = Button(frame, text="Ввод", command=lambda: your_input())
+        btn.pack()
+        win.wait_window(win)
+        return result_var.get()
+
+
+def saved_change():
+        def yes_function(window):
+            result_var.set(True)
+            window.destroy()
+        def no_function(window):
+            window.destroy()
+        win = Tk()
+        win['bg'] = '#ffe4b5'
+        win.title('Изменения сохранены!')
+        win.wm_attributes('-alpha', 1.0)
+        win.geometry('500x500')
+        canvas = Canvas(win, height=20, width=10)
+        canvas.pack()
+        result_var = BooleanVar()
+        title = Label(win, text='Хотите поменять что-либо еще?', bg='#ffe4b5', font=60)
+        title.pack()
+        frame = Frame(win, bg='white')
+        frame.place(relx=0.45, rely=0.40, relwidth=0.1, relheight=0.1)
+        btn = Button(frame, text="Да", command=lambda: yes_function(win))
+        btn.pack()
+        btn = Button(frame, text="Нет", command=lambda: no_function(win))
+        btn.pack()
+        win.wait_window(win)
+        return result_var.get()
